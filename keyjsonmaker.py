d1b0bc3e74e2723b2e00eed417762c5084687411
Brian Silvey <blujay270@gmail.com>
2015-10-25 05:20:16 -0400

    rewrote all of script.js. one major bug unsolved

--- keyjsonmaker.py
+++ keyjsonmaker.py
@@ -1,4 +1,4 @@
-f = open('key.json', 'w')
+f = open('noteKey.json', 'w')
 f.write('{"notes":[')
 freq = '0'
 i = 0
@@ -9,25 +9,25 @@ while currNum <= 8:
   for note in notes:
     if note == 'b' and currNum == 8:
       freq = raw_input('freq of ' + note + '-' + currNumString + ': ')
-      f.write('{"' + note + '-' + currNumString + '":' + freq + '}')   
+      f.write('{"note":"' + note + '-' + currNumString + '","freq":' + freq + '}')   
       break
     freq = raw_input('freq of ' + note + '-' + currNumString + ': ')
-    f.write('{"' + note + '-' + currNumString + '":' + freq + '},')
+    f.write('{"note":"' + note + '-' + currNumString + '","freq":' + freq + '},')
     if note == 'c':
       freq = raw_input('freq of ' + note + '^' + currNumString + ' / ' + 'd' + '_' + currNumString + ': ')
-      f.write('{"' + note + '^' + currNumString + '":' + freq + '},{"' + 'd' + '_' + currNumString + '":' + freq + '},')
+      f.write('{"note":"' + note + '^' + currNumString + '","freq":' + freq + '},{"note":"' + 'd' + '_' + currNumString + '","freq":' + freq + '},')
     elif note == 'd':
       freq = raw_input('freq of ' + note + '^' + currNumString + ' / ' + 'e' + '_' + currNumString + ': ')
-      f.write('{"' + note + '^' + currNumString + '":' + freq + '},{"' + 'e' + '_' + currNumString + '":' + freq + '},')
+      f.write('{"note":"' + note + '^' + currNumString + '","freq":' + freq + '},{"note":"' + 'e' + '_' + currNumString + '","freq":' + freq + '},')
     elif note == 'f':
       freq = raw_input('freq of ' + note + '^' + currNumString + ' / ' + 'g' + '_' + currNumString + ': ')
-      f.write('{"' + note + '^' + currNumString + '":' + freq + '},{"' + 'g' + '_' + currNumString + '":' + freq + '},')
+      f.write('{"note":"' + note + '^' + currNumString + '","freq":' + freq + '},{"note":"' + 'g' + '_' + currNumString + '","freq":' + freq + '},')
     elif note == 'g':
       freq = raw_input('freq of ' + note + '^' + currNumString + ' / ' + 'a' + '_' + currNumString + ': ')
-      f.write('{"' + note + '^' + currNumString + '":' + freq + '},{"' + 'a' + '_' + currNumString + '":' + freq + '},')
+      f.write('{"note":"' + note + '^' + currNumString + '","freq":' + freq + '},{"note":"' + 'a' + '_' + currNumString + '","freq":' + freq + '},')
     elif note == 'a':
       freq = raw_input('freq of ' + note + '^' + currNumString + ' / ' + 'b' + '_' + currNumString + ': ')
-      f.write('{"' + note + '^' + currNumString + '":' + freq + '},{"' + 'b' + '_' + currNumString + '":' + freq + '},')
+      f.write('{"note":"' + note + '^' + currNumString + '","freq":' + freq + '},{"note":"' + 'b' + '_' + currNumString + '","freq":' + freq + '},')
   currNum += 1
 f.write(']}')
 f.close()
--- script.js
+++ script.js
@@ -1,57 +1,144 @@
 var context = new AudioContext();
-var playing = 0;
 var currInsTable;
+var BPM = 0;
+var type = 0;
+var note = '';
+var noteDuration = 0;
+var pauseDuration = 0;
+var lines = [''];
+var score = [''];
 
 function loadJSON(file, callback) {
+  console.log("loadJSON");
   var xobj = new XMLHttpRequest();
   xobj.overrideMimeType("application/json");
   xobj.open('GET', file, true);
   xobj.onreadystatechange = function () {
     if (xobj.readyState == 4 && xobj.status == "200") {
-
       callback(xobj.responseText);
     }
   };
   xobj.send(null);
 }
 
-function voice(instrument, note) {
-  var oscillator;
-  var ins = instrument;
-  var nt = note;
+function loadIns(index) {
+  console.log("loadIns");
+  loadJSON("instruments.json", function(response) {
+    var json = JSON.parse(response);
+    var real = new Float32Array(json.table[Number(index)].real);
+    var imag = new Float32Array(json.table[Number(index)].imag);
+    currInsTable = context.createPeriodicWave(real, imag);
+  });
+}
 
-  this.changeInstrument = function (newInstrument) {
-    ins = newInstrument;
-  };
-  this.changeNote = function (newNote) {
-    nt = newNote;
-  };
-  function play(freq) {
+function loadFreq(callback) {
+  console.log("loadFreq");
+  if (note != '') {
+    loadJSON("noteKey.json", function(response) {
+      var json = JSON.parse(response);
+      for (var i = 0; i < json.notes.length; i++) {
+        if(json.notes[i].note == note) {
+          callback(json.notes[i].freq);
+        }
+      }
+    });
+  }
+  callback(0);
+}
+
+function loadType(callback) {
+  console.log("loadType");
+  if (type != '') {
+    loadJSON("typeKey.json", function(response) {
+      var json = JSON.parse(response);
+      for (var i = 0; i < json.types.length; i++) {
+        if(json.types[i].type == type) {
+          callback(json.types[i].num);
+        }
+      }
+    });
+  }
+  callback(0);
+}
+
+function produce(freq, callback) {
+  console.log("produce");
+  if (note.length == 3) {
     oscillator = context.createOscillator();
     oscillator.connect(context.destination);
     oscillator.setPeriodicWave(currInsTable);
     oscillator.frequency.value = Number(freq);
-    oscillator.start(context.currentTime);
+    setTimeout(function() {
+      oscillator.start(context.currentTime);
+    }, pauseDuration);
+    setTimeout(function() {
+      oscillator.stop(context.currentTime);
+    }, noteDuration);
   }
-  function stop() {
-    oscillator.stop(context.currentTime);
-  }
-  function changeIns(index, json) {
-    var real = new Float32Array(json.table[Number(index)].real);
-    var imag = new Float32Array(json.table[Number(index)].imag);
-    currInsTable = context.createPeriodicWave(real, imag);
-  }
-  function load(index) {
-    loadJSON("instruments.json", function(response) {
-      var json = JSON.parse(response);
-      changeIns(index, json)
-    });
-  }
-
-  load(0);
+  callback();
 }
 
-function parse(input) {
-  var readable = input.split();
-  // stuff
-};
+function play(callback) {
+  console.log("play");
+  loadFreq(function(freq) {
+    produce(freq, function() {
+      callback();
+    });
+  });
+}
+
+function initRead(input) {
+  console.log("initRead");
+  lines = input.split('\n');
+  BPM = lines[0].split('=')[1];
+  type = lines[1].split('=')[1];
+}
+
+function read() {
+  console.log("read");
+  loadType(function(numType) { 
+    score = lines[2].split("");
+    var tempNote = '';
+    while (note.length != 3 || score[0] == ' ') {
+      if (score[0] == '0' || score[0] == '1' || score[0] == '2' || score[0] == '3' || score[0] == '4' || score[0] == '5' || score[0] == '6' || score[0] == '7' || score[0] == '8') {
+        tempNote = tempNote.concat(score[0]);
+        score.splice(0, 1);
+        note = tempNote;
+        tempNote = '';
+      } else if (score[0] == ' ') {
+        noteDuration += ((BPM/numType)*1000)*(1/3);
+        score.splice(0, 1);
+      } else if (score[0] == '.') {
+        pauseDuration += ((BPM/numType)*1000)*(1/3);
+        score.splice(0, 1);
+      } else {
+        tempNote = tempNote.concat(score[0]);
+        score.splice(0, 1);
+      }
+    }
+  });
+}
+
+function resetValues(callback) {
+  console.log("resetValues");
+  note = '';
+  noteDuration = 0;
+  pauseDuration = 0;
+  callback();
+}
+
+function start() {
+  console.log("start");
+  loadIns(1);
+  if (document.getElementById("score").value != '') {
+    initRead(document.getElementById("score").value);
+    console.log("note = " + note + ", noteDuration = " + noteDuration + ", pauseDuration = " + pauseDuration + ", score = " + score + ", lines = " + lines);
+    while (score.length > 0) {
+      play(function() {
+        resetValues(function() {
+          read();
+        });
+      });
+    }
+  }
+}
